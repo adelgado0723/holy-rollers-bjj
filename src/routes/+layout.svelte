f2f1ded9d29c7808af1dee7c46a9c8f05814de5f
adelgado <adelgado0723@gmail.com>
2023-06-22 14:08:01 -0400

    moves images into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
+	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
 <AppShell>
@@ -16,9 +16,32 @@
 				><img alt="hamburger menu" src="/hamburger.svg" /></svelte:fragment
 			>
 			Holy Rollers BJJ
-			<svelte:fragment slot="trail"><img alt="hamburger menu" src="/calendar.svg" /></svelte:fragment>
+			<svelte:fragment slot="trail"
+				><img alt="hamburger menu" src="/calendar.svg" /></svelte:fragment
+			>
 		</AppBar></svelte:fragment
 	>
+
+	<div class="relative">
+		<div class="absolute left-0 top-0 z-[-1] -translate-x-5 -translate-y-12">
+			<div class="absolute left-1 top-1 z-[-2] h-48 w-48 rounded-full bg-primary-200" />
+			<Avatar
+				class=" z-[-1]"
+				src="BJJClub/Class2_06-02-2023.jpeg"
+				width="w-48"
+				rounded="rounded-full"
+			/>
+		</div>
+		<div class="absolute right-0 top-60 z-[-1] translate-x-5">
+			<div class="absolute left-1 top-1 z-[-2] h-48 w-48 rounded-full bg-primary-200" />
+			<Avatar
+				class=" z-[-1]"
+				src="BJJClub/Class4_06-02-2023.jpeg"
+				width="w-48"
+				rounded="rounded-full"
+			/>
+		</div>
+	</div>
 	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
 	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
 	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
 <div class=" container relative mx-auto flex h-full flex-col items-center justify-center gap-10">
@@ -7,24 +6,6 @@
 		<div class="text-6xl text-secondary-500 font-token">BELLEVIEW UMC</div>
 		<div class="text-8xl font-heading-token">Holy Rollers</div>
 		<div class="text-6xl text-secondary-500 font-token">JIU JITSU CLUB</div>
-	</div>
-	<div class="absolute left-0 top-0 z-[-1] -translate-x-28 -translate-y-12">
-		<div class="absolute left-1 top-1 z-[-2] h-48 w-48 rounded-full bg-primary-200" />
-		<Avatar
-			class=" z-[-1]"
-			src="BJJClub/Class2_06-02-2023.jpeg"
-			width="w-48"
-			rounded="rounded-full"
-		/>
-	</div>
-	<div class="absolute right-0 top-52 z-[-1] translate-x-28">
-		<div class="absolute left-1 top-1 z-[-2] h-48 w-48 rounded-full bg-primary-200" />
-		<Avatar
-			class=" z-[-1]"
-			src="BJJClub/Class4_06-02-2023.jpeg"
-			width="w-48"
-			rounded="rounded-full"
-		/>
 	</div>
 
 	<ol class="breadcrumb flex h-full flex-row justify-center">
