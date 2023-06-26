0a35796c9fb39d3fa68ee0e4e78ac6f44cf0a00a
adelgado <adelgado0723@gmail.com>
2023-06-26 17:28:29 -0400

    working on sticky nav effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,42 +9,29 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<AppShell>
-	<svelte:fragment slot="header"
-		><AppBar class="text-2xl md:text-4xl p-0 m-0" padding='p-1 md:p-2' >
-			<svelte:fragment slot="lead"
-				><img alt="hamburger menu" class="w-10 md:w-14 m-0 p-0" src="/hamburger.svg" /></svelte:fragment
-			>
-			Holy Rollers BJJ
-			<svelte:fragment slot="trail"
-				><img alt="hamburger menu" class="w-10 md:w-14 m-0 p-0"src="/calendar.svg" /></svelte:fragment
-			>
-		</AppBar></svelte:fragment
-	>
+<AppShell regionPage="relative" slotPageHeader="relative  top-0 z-10">
+	<!-- <svelte:fragment slot="header"> -->
+	<!-- 	<AppBar class="sticky top-0 text-2xl md:text-4xl" padding="p-1 md:p-2"> -->
+	<!-- 		<svelte:fragment slot="lead" -->
+	<!-- 			><img -->
+	<!-- 				alt="hamburger menu" -->
+	<!-- 				class="m-0 w-10 p-0 md:w-14" -->
+	<!-- 				src="/hamburger.svg" -->
+	<!-- 			/></svelte:fragment -->
+	<!-- 		> -->
+	<!-- 		Holy Rollers BJJ -->
+	<!-- 		<svelte:fragment slot="trail" -->
+	<!-- 			><img -->
+	<!-- 				alt="hamburger menu" -->
+	<!-- 				class="m-0 w-10 p-0 md:w-14" -->
+	<!-- 				src="/calendar.svg" -->
+	<!-- 			/></svelte:fragment -->
+	<!-- 		> -->
+	<!-- 	</AppBar></svelte:fragment -->
+	<!-- > -->
 
-	<div class="relative">
-		<div class="absolute left-0 top-0 z-[-1] -translate-x-5 -translate-y-12">
-			<div class="absolute left-1 top-1 z-[-2] h-32 w-32 md:h-48 md:w-48 rounded-full bg-primary-200" />
-			<Avatar
-				class=" z-[-1]"
-				src="BJJClub/Class2_06-02-2023.jpeg"
-				width="w-32 md:w-48"
-				rounded="rounded-full"
-			/>
-		</div>
-		<div class="absolute right-0 top-60 z-[-1] translate-x-5">
-			<div class="absolute left-1 top-1 z-[-2] h-32 w-32 md:h-48 md:w-48 rounded-full bg-primary-200" />
-			<Avatar
-				class=" z-[-1]"
-				src="BJJClub/Class4_06-02-2023.jpeg"
-				width="w-32 md:w-48"
-				rounded="rounded-full"
-			/>
-		</div>
-	</div>
 	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
 	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
-	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
 	<!-- Router Slot -->
 	<slot />
 	<!-- ---- / ---- -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,102 @@
 <script>
+	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<div class=" container relative mx-auto flex h-full flex-col items-center justify-center gap-10">
-	<div class="m-4 mt-[4.5rem] flex flex-col items-center justify-center gap-6 md:m-8 md:mt-2 md:gap-8">
-		<div class="text-4xl text-secondary-500 font-token md:text-6xl">BELLEVIEW UMC</div>
-		<div class="text-5xl font-heading-token md:text-8xl">Holy Rollers</div>
-		<div class="text-4xl text-secondary-500 font-token md:text-6xl">JIU JITSU CLUB</div>
-	</div>
+<div
+	class=" container relative mx-auto flex h-full w-full flex-col items-center justify-center gap-10 scroll-smooth"
+>
+	<div class="relative mt-32 flex h-full w-full flex-col items-center justify-center">
+		<div class="absolute left-0 top-0 z-[-1] -translate-x-5 -translate-y-12">
+			<div
+				class="absolute left-1 top-1 z-[-2] h-32 w-32 rounded-full bg-primary-200 md:h-48 md:w-48"
+			/>
+			<Avatar
+				class=" z-[-1]"
+				src="BJJClub/Class2_06-02-2023.jpeg"
+				width="w-32 md:w-48"
+				rounded="rounded-full"
+			/>
+		</div>
 
-	<ol class="breadcrumb flex h-full flex-row justify-center mt-16">
+		<div
+			class="m-4 mt-[4.5rem] flex flex-col items-center justify-center gap-6 md:m-8 md:mt-2 md:gap-8"
+		>
+			<div class="text-4xl text-secondary-500 font-token md:text-6xl">BELLEVIEW UMC</div>
+			<div class="text-5xl font-heading-token md:text-8xl">Holy Rollers</div>
+			<div class="text-4xl text-secondary-500 font-token md:text-6xl">JIU JITSU CLUB</div>
+		</div>
+
+		<div class="absolute right-0 top-60 z-[-1] translate-x-5">
+			<div
+				class="absolute left-1 top-1 z-[-2] h-32 w-32 rounded-full bg-primary-200 md:h-48 md:w-48"
+			/>
+			<Avatar
+				class=" z-[-1]"
+				src="BJJClub/Class4_06-02-2023.jpeg"
+				width="w-32 md:w-48"
+				rounded="rounded-full"
+			/>
+		</div>
+	</div>
+	<ol
+		class="breadcrumb sticky top-0 m-24 box-border flex h-48 w-[100vw] flex-row justify-center bg-secondary-500"
+	>
 		<li class="crumb">
 			<a href="#who-we-are" class="anchor">Who We Are</a>
 		</li>
 		<li class="crumb-separator" aria-hidden>/</li>
 		<li class="crumb"><a class="anchor" href="/schedule">Schedule</a></li>
+		<li class="crumb-separator" aria-hidden>/</li>
+		<li class="crumb"><a class="anchor" href="#location">Location</a></li>
+		<li class="crumb-separator" aria-hidden>/</li>
+		<li class="crumb"><a class="anchor" href="#class-overview">Class Overview</a></li>
+		<li class="crumb-separator" aria-hidden>/</li>
+		<li class="crumb"><a class="anchor" href="#register">Register</a></li>
+		<li class="crumb-separator" aria-hidden>/</li>
+		<li class="crumb"><a class="anchor" href="#contact-us">Contact Us</a></li>
 	</ol>
 
-	<div class="m-3 md:m-4">
-		<h1 id="who-we-are">Who we are</h1>
+	<div id="who-we-are" class="m-3 w-full pt-14 md:m-4">
+		<h1>Who we are</h1>
 		<p class="text-lg">
 			Hosted by Belleview United Methodist Church, the Holy Rollers BJJ club is a faith based
 			martial arts programed aimed at teaching children ages 4-16 discipline and self-defense
 		</p>
 	</div>
 
-	<div class="m-3 w-full md:m-4">
+	<div id="schedule" class="m-3 w-full pt-14 md:m-4">
 		<h1>Schedule</h1>
+		<p class="m-3 md:m-4">This is a test paragraph... calendar. Add to calender option?</p>
+	</div>
+
+	<div id="location" class="m-3 w-full pt-14 md:m-4">
+		<h1>Location</h1>
+		<p class="m-3 md:m-4">This is a test paragraph... Include a map?</p>
+	</div>
+
+	<div id="class-overview" class="m-3 w-full pt-14 md:m-4">
+		<h1>Class Overview</h1>
+		<p class="m-3 md:m-4">
+			This is a test paragraph... include a description of the class structure
+		</p>
+	</div>
+
+	<div id="register" class="m-3 w-full pt-14 md:m-4">
+		<h1>Register</h1>
+		<p class="m-3 md:m-4">This is a test paragraph... download registration forms</p>
+	</div>
+
+	<div id="contact-us" class="m-3 w-full pt-14 md:m-4">
+		<h1>Contact Us</h1>
+		<p class="m-3 md:m-4">This is a test paragraph... include a contact form?</p>
+	</div>
+
+	<div id="test" class="m-3 w-full pt-14 md:m-4">
+		<h1>Test</h1>
+		<p class="m-3 md:m-4">This is a test paragraph</p>
+	</div>
+	<div id="test" class="m-3 w-full pt-14 md:m-4">
+		<h1>Test</h1>
 		<p class="m-3 md:m-4">This is a test paragraph</p>
 	</div>
 </div>
